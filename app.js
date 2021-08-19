9314bf25f99b8b704e0e07732735524b0d9401e9
smccourtb <shawnmccourtb@gmail.com>
2021-08-19 09:38:37 -0400

    refactor return statements from string to variables to clean it up a bit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
-
+const playerSelection = window.prompt('Choose rock, paper, or scissors', "rock").toLowerCase()
+// const computerSelection = computerPlay()
 function computerPlay() {
     let options = ['Rock', 'Paper', 'Scissors'];
     // pick a random choice from options
@@ -7,20 +8,42 @@ function computerPlay() {
 }
 
 function playRound(computerSelection, playerSelection) {
-    let player = playerSelection.toLowerCase();
-    if (player === 'rock') {
+    let winMessage = `You Win! ${playerSelection} beats ${computerSelection}`
+    let loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`
+    let drawMessage = "It's a draw! Try again!"
+
+    if (playerSelection === 'rock') {
         if (computerSelection === 'scissors') {
-            return 'You Win!'
+            return winMessage
         }
         else if (computerSelection === 'paper') {
-            return 'You Lose'
+            return loseMessage
         }
         else {
-            return 'Draw'
+            return drawMessage
         }
     }
-    
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return winMessage
+        }
+        else if (computerSelection === 'rock') {
+            return loseMessage
+        }
+        else {
+            return drawMessage
+        }
+    }
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return winMessage
+        }
+        else if (computerSelection === 'scissors') {
+            return loseMessage
+        }
+        else {
+            return drawMessage
+        }
+    }
 }
 
-const playerSelection = "rock"
-// const computerSelection = computerPlay()
