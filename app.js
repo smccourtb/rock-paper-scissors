a5bd1af48b1468899c5bbb79427495276c392571
smccourtb <shawnmccourtb@gmail.com>
2021-08-19 09:13:49 -0400

    add computer choice

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-function rollDie() {
-    let roll = Math.floor(Math.random() * 6) + 1;
-    console.log(`Rolled: ${roll}`);
+
+
+function computerPlay() {
+    let options = ['Rock', 'Paper', 'Scissors'];
+    // pick a random choice from options
+    return options[Math.floor(Math.random() * options.length)].toLowerCase()
 }
+
+function playRound(computerSelection, playerSelection) {
+    let player = playerSelection.toLowerCase();
+    if (player === 'rock') {
+        if (computerSelection === 'scissors') {
+            return 'You Win!'
+        }
+        else if (computerSelection === 'paper') {
+            return 'You Lose'
+        }
+        else {
+            return 'Draw'
+        }
+    }
+    
+}
+
+const playerSelection = "rock"
+// const computerSelection = computerPlay()
