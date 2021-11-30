5074e8a9a8ed9e7a013ff0f8335a0d56579d10cd
smccourtb <shawnmccourtb@gmail.com>
2021-11-29 22:24:35 -0500

    import react and create header and choice components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./components/Header";
+import Choice from "./components/Choice";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Choice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Choice.js
@@ -0,0 +1,11 @@
+function Choice() {
+    return(
+        <div className = "rock">
+            <div className="rock-bg">
+                <i class="far fa-hand-rock hand-rock"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Choice
