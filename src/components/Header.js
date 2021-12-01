547c6a8f04df80fe43f01c7c1966477a6c91bf2e
smccourtb <shawnmccourtb@gmail.com>
2021-12-01 12:20:40 -0500

    moved header styles in separate file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import basicLogo from "../images/logo.svg"
-
+import "../styles/Header.css"
 function Header() {
     return(
         <header>
-            <img src={basicLogo} alt="logo"/>
+            <img className="header__logo" src={basicLogo} alt="logo"/>
+            <div className="header__score-container">
+                <small>SCORE</small>
+                <p className="header__score">0</p>
+            </div>
         </header>
     )
 }
